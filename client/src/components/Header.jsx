bd4ba8d5a0fe5f9ea6c687dcc59b4a574937972a
nithishkumarsaravanan <nithishkumar2320005@gmail.com>
2025-07-28 16:06:48 +0530

    header changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,25 @@
-import './header.css'
+import '../css/header.css'
+import logo from '../images/logo.png'
 function Header(){
 
     return(
         <>
-        <div className="header">
-            <img src=""/>
-                 
+        <div className="body">
             
+        <div className="header">
+            <img id="logo"src={logo}/>
+            <p id="title">Job portal</p>
+            <div className="navlist">
+
+            <ul id="list-items">
+                <li>Jobs</li>
+                <li>Company</li>
+                <li>Services</li>
+            </ul>
+            <button id="login-btn">Login</button>
+            </div>
+            
+        </div>
         </div>
         </>
     );
